6e94cbbec498af5a34aee78057d066d217f680e1
Alexandre Djun Igari <djun_igari@hotmail.com>
2022-03-07 19:37:05 +0900

    Add Header component and Banner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import Image from "next/image"
+import { MenuIcon, SearchIcon, ShoppingCartIcon } from "@heroicons/react/outline"
 
 function Header() {
   return (
     <header>
       {/* Top Nav */}
-      <div className="flex items-center">
-        <di>
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
             src="https://links.papareact.com/f90"
             width={150}
@@ -13,11 +14,39 @@ function Header() {
             objectFit="contain"
             className="cursor-pointer"
           />
-        </di>
+        </div>
+        {/* Search */}
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input className="p-2 px-4 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none" type="text" />
+          <SearchIcon className="h-12 p-4" />
+        </div>
+        {/* Right */}
+        <div className="flex items-center space-x-6 text-white text-xs mx-6 whitespace-nowrap">
+          <div className="link">
+            <p>Hello Sonny Sangha</p>
+            <p className="font-extrabold md:text-sm">Account & Lists</p>
+          </div>
+          <div className="link">
+            <p>Returns</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
+          </div>
+          <div className="relative link flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 rounded-full text-center text-black font-bold">0</span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
+          </div>
+        </div>
       </div>
       {/* Bottom NAv */}
-      <div>
-
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="link hidden lg:inline-flex">ELectronics</p>
       </div>
     </header>
   )
